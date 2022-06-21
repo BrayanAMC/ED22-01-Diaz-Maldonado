1a95218b9c47d3297fc10c08c2d38d2157910be4
pinhasaurio <brayan.maldonado@alumnos.ucn.cl>
2022-06-21 18:26:28 -0400

    implementacion de incorporacion de video(no identificacion por cuadrado)

---
+++ Taller_opencv/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Brayann/opencv_test2/ED22-01-Diaz-Maldonado/Taller_opencv
+# Build directory: C:/Users/Brayann/opencv_test2/ED22-01-Diaz-Maldonado/Taller_opencv/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
